b65bd79e89780802af52ceadd7585d5f757e9018
Diego Viana <diego.p.oder@hotmail.com>
2020-11-19 17:45:22 -0300

    Create auth.js

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/framework/src/Env')} */
+const Env = use('Env')
+
+module.exports = {
+  /*
+  |--------------------------------------------------------------------------
+  | Authenticator
+  |--------------------------------------------------------------------------
+  |
+  | Authentication is a combination of serializer and scheme with extra
+  | config to define on how to authenticate a user.
+  |
+  | Available Schemes - basic, session, jwt, api
+  | Available Serializers - lucid, database
+  |
+  */
+  authenticator: 'jwt',
+
+}
