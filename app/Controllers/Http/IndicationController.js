be0729a884cb178e8dfac66ee9c322feae58d913
Diego Viana <diego.p.oder@hotmail.com>
2021-01-14 18:58:15 -0300

    Update IndicationController.js

--- app/Controllers/Http/IndicationController.js
+++ app/Controllers/Http/IndicationController.js
@@ -20,16 +20,16 @@ class IndicationController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, view }) {
     try {
       const indications = await Database
         .raw('select school_id, count(*) as indications \
               from indications \
               group by (school_id)'
-      )
+        )
       Database.close()
 
-      return response.json(indications.rows) 
+      return response.json(indications.rows)
     } catch (error) {
       return response.badRequest(`Erro: ${error.name}\nMensagem: ${error.message}`)
     }
@@ -44,7 +44,7 @@ class IndicationController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
+  async create({ request, response, view }) {
   }
 
   /**
@@ -55,7 +55,7 @@ class IndicationController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
     const indicationData = request.only(['school_id', 'uid', 'period'])
 
     const trx = await Database.beginTransaction()
@@ -67,14 +67,14 @@ class IndicationController {
 
       return response.json({
         success: true,
-        data: { 
+        data: {
           indication: {
             ...indication.$attributes
           }
         },
         message: 'Indicacao criada com sucesso.'
       })
-    } catch(error) {
+    } catch (error) {
       return response.badRequest(`Erro: ${error.name}\nMensagem: ${error.message}`)
     }
   }
@@ -88,17 +88,17 @@ class IndicationController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
     try {
       const indication = await Database
-        .raw('select school_id, count(*) as indications \
-              from indications \
-              where (period =' + params.period + 'AND school_id =' + params.school_id + ' ) \
-              group by (school_id)'
-      )
+        .raw('select i.school_id, count(*) as indications, s."socialReason", s."latitudeSchool", s."longitudeSchool", s."emailSchool", s."addressSchool", p."numberPhone" \
+              from indications i join schools s on i.school_id = s.id join phones p on s.id = p.school_id \
+              where (i.period =' + params.period + 'AND i.school_id =' + params.school_id + ' ) \
+              group by (i.school_id, s.id, p.id)'
+        )
 
       Database.close()
-      return response.json(indication.rows) 
+      return response.json(indication.rows)
     } catch (error) {
       return response.badRequest(`Erro: ${error.name}\nMensagem: ${error.message}`)
     }
@@ -113,7 +113,7 @@ class IndicationController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
+  async edit({ params, request, response, view }) {
   }
 
   /**
@@ -124,7 +124,7 @@ class IndicationController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
   }
 
   /**
@@ -135,7 +135,7 @@ class IndicationController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
     const indication = await Indication.findByOrFail('id', params.id)
     try {
       await indication.delete()
